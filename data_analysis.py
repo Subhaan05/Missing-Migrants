a99d7dd2107092c59d213a4a38bdd02ab880f9a2
Subhaan05 <subhaanazam@outlook.com>
2024-12-18 18:41:13 +0000

    Dropped irrelevant columns

--- data_analysis.py
+++ data_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load dataset into a df
-file_path = '/Users/subhaan/Desktop/Code/Missing_Migrants_Global_Figures_allData.csv'
+file_path = 'Missing_Migrants_Global_Figures_allData.csv'
 data = pd.read_csv(file_path)
 
 # Display information about the dataset
@@ -11,3 +11,10 @@ print(data.info())
 print(data.columns)
 print(data.shape)
 
+# Columns dropping
+
+columns_to_remove = ['Main ID', 'Incident ID', 'UNSD Geographical Grouping', 'URL', 'Source Quality']
+data = data.drop(columns=columns_to_remove)
+
+print(data.columns)
+print(data.shape)
