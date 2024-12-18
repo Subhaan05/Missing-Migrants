d7751c297a877509a579f6052761d35027be1f60
Subhaan05 <subhaanazam@outlook.com>
2024-12-18 18:49:34 +0000

    Columm rennaming added to suit the data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -13,8 +13,29 @@ print(data.shape)
 
 # Columns dropping
 
-columns_to_remove = ['Main ID', 'Incident ID', 'UNSD Geographical Grouping', 'URL', 'Source Quality']
+columns_to_remove = ['Main ID', 'Incident ID', 'UNSD Geographical Grouping', 'URL', 'Source Quality', 'Information Source']
 data = data.drop(columns=columns_to_remove)
+
+# Columns renaming to suit visualisations
+columns_new_names = {
+    'Region of Incident': 'Incident Region',
+    'Date of Incident': 'Incident Date',
+    'Year of Incident': 'Incident Year',
+    'Number of Dead': 'Deaths Count',
+    'Minimum Estimated Number of Missing': 'Minimum Missing Count',
+    'Total Number of Dead and Missing': 'Total Missing and Dead',
+    'Number of Survivors': 'Survivors Count',
+    'Number of Females': 'Females Count',
+    'Number of Males': 'Males Count',
+    'Number of Children': 'Children Count',
+    'Country of Origin': 'Origin Country',
+    'Region of Origin': 'Origin Region',
+    'Country of Incident': 'Incident Country',
+    'Location of Death': 'Incident Location',
+}
+
+# Applying column renaming
+data.rename(columns=columns_new_names, inplace=True)
 
 print(data.columns)
 print(data.shape)
